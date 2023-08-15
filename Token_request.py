c70360692265d8d23077d67293fc2e4b401d5fc8
Eberth Alarcón <heyeberth@gmail.com>
2023-08-15 18:43:28 -0400

    Monitoreo de RED cada 5 minutos API Controller Server y cantidad de dispositovos en la red

--- Token_request.py
+++ Token_request.py
@@ -39,47 +39,50 @@ except Exception as err:
 #print("***" *200)
 
 #GET http://apic-ip-address/api/class/topSystem.json
-while True:
-    # Monitorar la RED cada 5 minutos, en busca de algun evento extraño.
-    time.sleep(300) #300 segundo equivalen a 5 minutos del tiempo que necesitamos.
 
 url_top = "https://10.10.20.14/api/class/topSystem.json"
 cabecera_top= {"content-type":"application/json"}
 webtoken_top= { "APIC-Cookie": Token_request(conf.user, conf.password)}
-respuesta_top= requests.get(url_top, headers=cabecera_top, cookies=webtoken_top, verify=False)
-respuesta_top_json= respuesta_top.json()
-total_equipos_json= respuesta_top_json["totalCount"]
-total_equipos_json = int(total_equipos_json)
-#print(respuesta_top_json)
-try:
-    name = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["name"]
-    tiempo = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["modTs"]
-    ip = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["address"]
-    mac = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["fabricMAC"]
-    state = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["state"]
-except Exception as err:
-    print(" - -> Error en la URL, por favor corrobar dicha informaciòn < - -")
-    exit(1)
-print("")
-print(" Cantidad de equipos operativos de la RED: ", total_equipos_json)
-print("")
-print("////"*20)
-print("              Identificacion Servidor Controlador Disponible")
-print("////"*20)
-print("- Nombre del servidor :", name)
-print("- Direcciòn IP :", ip)
-print("- Direcciòn MAC :", mac)
-print("- Estado actual :", state)
-print("- Tiempo operativo :", tiempo)
-print("*-*-"*20)
-print("")
 
-#Si existe un número mayor que 4 de equipos en la red debe ser notificado.
-if total_equipos_json > 4:
-    print(" ## ALERTA ## Se ha detectado nuevos equipos dentro de nuestra red")
-    print(" ## ALERTA ## Se ha notificado al administrador de la red")
+while True:
+    # Monitorar la RED cada 5 minutos, en busca de algun evento extraño.
 
-#Si el servidor Controlador Cae, deberà ser notificado.
-if state != "in-service":
-    print(" ## ALERTA ## Se ha detectado que el servidor CONTROLADOR no esta disponible")
-    print(" ## ALERTA ## Se ha notificado al administrador de la red")
+    respuesta_top= requests.get(url_top, headers=cabecera_top, cookies=webtoken_top, verify=False)
+    respuesta_top_json= respuesta_top.json()
+    total_equipos_json= respuesta_top_json["totalCount"]
+    total_equipos_json = int(total_equipos_json)
+    #print(respuesta_top_json)
+    try:
+        name = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["name"]
+        tiempo = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["modTs"]
+        ip = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["address"]
+        mac = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["fabricMAC"]
+        state = respuesta_top_json["imdata"][0]["topSystem"]["attributes"]["state"]
+    except Exception as err:
+        print(" - -> Error en la URL, por favor corrobar dicha informaciòn < - -")
+        exit(1)
+    print("")
+    print(" Cantidad de equipos operativos de la RED: ", total_equipos_json)
+    print("")
+    print("////"*20)
+    print("              Identificacion Servidor Controlador Disponible")
+    print("////"*20)
+    print("- Nombre del servidor :", name)
+    print("- Direcciòn IP :", ip)
+    print("- Direcciòn MAC :", mac)
+    print("- Estado actual :", state)
+    print("- Tiempo operativo :", tiempo)
+    print("*-*-"*20)
+    print("")
+
+    #Si existe un número mayor que 4 de equipos en la red debe ser notificado.
+    if total_equipos_json > 4:
+        print(" ## ALERTA ## Se ha detectado nuevos equipos dentro de nuestra red")
+        print(" ## ALERTA ## Se ha notificado al administrador de la red")
+
+    #Si el servidor Controlador Cae, deberà ser notificado.
+    if state != "in-service":
+        print(" ## ALERTA ## Se ha detectado que el servidor CONTROLADOR no esta disponible")
+        print(" ## ALERTA ## Se ha notificado al administrador de la red")
+
+    time.sleep(5)  # 300 segundos equivalen a 5 minutos del tiempo que necesitamos.
